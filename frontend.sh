c230abaf96b92c8b4a79d44f5fa010b3eb3cf78b
koteswaraRao559 <koteswararao1999@gmail.com>
2024-03-04 09:49:05 +0530

    4th commit

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,22 @@
 echo -e "\e[31m installing nginx \e[0m"
-dnf install nginx -y
+dnf install nginx -y &>> /temp/expense.log
 
 echo -e "\e[32m copying file  \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf &>> /temp/expense.log
 
 echo -e "\e[33m removing file  \e[0m"
 rm -rf /usr/share/nginx/html/*
 
 echo -e "\e[35m Downloading Zip file \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>> /temp/expense.log
 
 
 cd /usr/share/nginx/html
 echo -e "\e[34m unziping  \e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>> /temp/expense.log
 
 echo -e "\e[31m starting nginx services \e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx&>> /temp/expense.log
+systemctl restart nginx&>> /temp/expense.log
 
 
