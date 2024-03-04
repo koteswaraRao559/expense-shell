9bdf877ceabb60fd1113fb8bee9ad69ec8f81867
koteswaraRao559 <koteswararao1999@gmail.com>
2024-03-04 15:03:41 +0530

    4th commit

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,31 @@
-echo -e "\e[31m installing nginx \e[0m"
-dnf install nginx -y &>> /tmp/expense.log
+log_file=/tmp/expense.log
+color= "\e[31m"
 
-echo -e "\e[32m copying file  \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>> /tmp/expense.log
+echo -e "${color} installing nginx \e[0m"
+dnf install nginx -y &>> log_file
+echo $?
 
-echo -e "\e[33m removing file  \e[0m"
+echo -e "${color} copying file  \e[0m"
+cp expense.conf /etc/nginx/default.d/expense.conf &>> log_file
+echo $?
+
+echo -e "${color} removing file  \e[0m"
 rm -rf /usr/share/nginx/html/*
+echo $?
 
-echo -e "\e[35m Downloading Zip file \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>> /tmp/expense.log
-
+echo -e "${color} Downloading Zip file \e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>> log_file
+echo $?
 
 cd /usr/share/nginx/html
-echo -e "\e[34m unziping  \e[0m"
-unzip /tmp/frontend.zip &>> /tmp/expense.log
+echo -e "${color} unziping  \e[0m"
+unzip /tmp/frontend.zip &>> log_file
+echo $?
 
-echo -e "\e[31m starting nginx services \e[0m"
-systemctl enable nginx&>> /tmp/expense.log
-systemctl restart nginx&>> /tmp/expense.log
+echo -e "${color} starting nginx services \e[0m"
+systemctl enable nginx&>> log_file
+echo $?
+systemctl restart nginx&>> log_file
+echo $?
 
 
