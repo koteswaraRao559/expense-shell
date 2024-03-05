2b3c43355bb8c7cacf2159ca08f55bbef7d6c1f0
koteswaraRao559 <koteswararao1999@gmail.com>
2024-03-05 20:39:26 +0530

    4th commit

--- backend.sh
+++ backend.sh
@@ -3,7 +3,7 @@ color="\e[31m"
 
 echo -e "${color} Disabling Default node.js \e[0m"
 dnf module disable nodejs -y &>>log_file
-if[ $? -eq 0];then
+if [ $? -eq 0]; then
   echo -e "\e[32m sucess \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
@@ -12,16 +12,16 @@ if
 echo -e "${color} Enabling & installing Node.js of version 18 \e[0m"
 dnf module enable nodejs:18 -y &>>log_file
 dnf install nodejs -y &>>log_file
-if[ $? -eq 0];then
+if [ $? -eq 0]; then
   echo -e "\e[32m sucess \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
-if
+iff
 
 echo -e "${color} Coping backend.service file \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>log_file
 echo $?
-if[ $? -eq 0];then
+if [ $? -eq 0]; then
   echo -e "\e[32m sucess \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
@@ -31,7 +31,7 @@ echo -e "${color} Adding User \e[0m"
 id expense &>>log_file
 if [ $? -ne 0]; then
   useradd expense &>>log_file
-  if[ $? -eq 0];then
+  if [ $? -eq 0]; then
     echo -e "\e[32m sucess \e[0m"
   else
     echo -e "\e[33m Failure \e[0m"
@@ -40,7 +40,7 @@ fi
 
 echo -e "${color} creating a directory \e[0m"
 mkdir /app
-if[ $? -eq 0];then
+if [ $? -eq 0]; then
   echo -e "\e[32m sucess \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
@@ -51,7 +51,7 @@ curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 echo -e "${color} Extracting Application content\e[0m"
 unzip /tmp/backend.zip
-if[ $? -eq 0];then
+if [ $? -eq 0]; then
   echo -e "\e[32m sucess \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
@@ -60,7 +60,7 @@ if
 cd /app
 echo -e "${color} Download nodejs Dependencies \e[0m"
 npm install &>>log_file
-if[ $? -eq 0];then
+if [ $? -eq 0]; then
   echo -e "\e[32m sucess \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
@@ -70,17 +70,16 @@ echo -e "${color} Installing mysql to load Schema \e[0m"
 dnf install mysql -y &>>log_file
 echo -e "${color} Load Schema \e[0m"
 mysql -h mysql-dev.kdevops59.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
-if[ $? -eq 0];then
+if [ $? -eq 0]; then
   echo -e "\e[32m sucess \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
 if
-
 echo -e "${color} Stating Backend services \e[0m"
 systemctl daemon-reload &>>log_file
 systemctl enable backend &>>log_file
 systemctl start backend &>>log_file
-if[ $? -eq 0];then
+if [ $? -eq 0]; then
   echo -e "\e[32m sucess \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
