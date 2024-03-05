8f7144780ab3f6c564cc8388c793893dd1180532
koteswaraRao559 <koteswararao1999@gmail.com>
2024-03-05 22:16:45 +0530

    4th commit

--- backend.sh
+++ backend.sh
@@ -4,7 +4,7 @@ color="\e[31m"
 echo -e "${color} Disabling Default node.js \e[0m"
 dnf module disable nodejs -y &>>log_file
 if [ $? -eq 0 ]; then
-  echo -e "\e[32m sucess \e[0m"
+  echo -e "\e[32m success \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
 fi
@@ -13,7 +13,7 @@ echo -e "${color} Enabling & installing Node.js of version 18 \e[0m"
 dnf module enable nodejs:18 -y &>>log_file
 dnf install nodejs -y &>>log_file
 if [ $? -eq 0 ]; then
-  echo -e "\e[32m sucess \e[0m"
+  echo -e "\e[32m success \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
 fi
@@ -21,7 +21,7 @@ fi
 echo -e "${color} Coping backend.service file \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>log_file
 if [ $? -eq 0 ]; then
-  echo -e "\e[32m sucess \e[0m"
+  echo -e "\e[32m success \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
 fi
@@ -29,10 +29,10 @@ fi
 
 id expense &>>log_file
 if [ $? -ne 0 ]; then
-  echo -e "${color} Adding User \e[0m"
+  echo -e "${color} Add Application User \e[0m"
   useradd expense &>>log_file
   if [ $? -eq 0 ]; then
-    echo -e "\e[32m sucess \e[0m"
+    echo -e "\e[32m success \e[0m"
   else
     echo -e "\e[33m Failure \e[0m"
   fi
@@ -42,7 +42,7 @@ if [ -d /app ]; then
   echo -e "${color} creating application directory \e[0m"
   mkdir /app &>>log_files
   if [ $? -eq 0 ]; then
-    echo -e "\e[32m sucess \e[0m"
+    echo -e "\e[32m success \e[0m"
   else
     echo -e "\e[33m Failure \e[0m"
   fi
@@ -51,7 +51,7 @@ fi
 echo -e "${color} Download Application content  \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
 if [ $? -eq 0 ]; then
-  echo -e "\e[32m sucess \e[0m"
+  echo -e "\e[32m success \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
 fi
@@ -60,7 +60,7 @@ echo -e "${color} Extracting Application content\e[0m"
 cd /app &>>log_file
 unzip /tmp/backend.zip &>>log_file
 if [ $? -eq 0 ]; then
-  echo -e "\e[32m sucess \e[0m"
+  echo -e "\e[32m success \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
 fi
@@ -68,7 +68,7 @@ fi
 echo -e "${color} Download nodejs Dependencies \e[0m"
 npm install &>>log_file
 if [ $? -eq 0 ]; then
-  echo -e "\e[32m sucess \e[0m"
+  echo -e "\e[32m success \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
 fi
@@ -78,7 +78,7 @@ dnf install mysql -y &>>log_file
 echo -e "${color} Load Schema \e[0m"
 mysql -h mysql-dev.kdevops59.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
 if [ $? -eq 0 ]; then
-  echo -e "\e[32m sucess \e[0m"
+  echo -e "\e[32m success \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
 fi
@@ -87,7 +87,7 @@ systemctl daemon-reload &>>log_file
 systemctl enable backend &>>log_file
 systemctl start backend &>>log_file
 if [ $? -eq 0 ]; then
-  echo -e "\e[32m sucess \e[0m"
+  echo -e "\e[32m success \e[0m"
 else
   echo -e "\e[33m Failure \e[0m"
 fi
