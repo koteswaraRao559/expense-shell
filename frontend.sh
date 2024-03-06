794f36d7282a3a1adb163a8afd33a177d879e6e3
koteswaraRao559 <koteswararao1999@gmail.com>
2024-03-06 20:09:46 +0530

    4th commit

--- frontend.sh
+++ frontend.sh
@@ -4,26 +4,25 @@ echo -e "${color} installing nginx \e[0m"
 dnf install nginx -y &>> log_file
 status_check
 
-echo -e "${color} copying file  \e[0m"
+echo -e "${color} copying Expense config file  \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>> log_file
 status_check
 
-echo -e "${color} removing file  \e[0m"
+echo -e "${color} clear old nginx file  \e[0m"
 rm -rf /usr/share/nginx/html/*
 status_check
 
-echo -e "${color} Downloading Zip file \e[0m"
+echo -e "${color} Download front application code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>> log_file
 status_check
 
-cd /usr/share/nginx/html
-echo -e "${color} unziping  \e[0m"
+echo -e "${color} Extracting and downloading applicaion content  \e[0m"
+cd /usr/share/nginx/html &>>log_file
 unzip /tmp/frontend.zip &>> log_file
 status_check
 
 echo -e "${color} starting nginx services \e[0m"
 systemctl enable nginx&>> log_file
-status_check
 systemctl restart nginx&>> log_file
 status_check
 
--- mysql.sh
+++ mysql.sh
@@ -1,23 +1,29 @@
-log_file=/tmp/expense.log
-color="\e[31m"
+source common.sh
+
+if [ -z "$1" ]; then
+  echo password input missing
+  exit
+fi
+MY_SQL_ROOT_PASSWORD=$1
 
 echo -e "${color} Disabling Default mysql \e[0m"
 dnf module disable mysql -y &>>log_file
-echo $?
+status_check
 
 echo -e "${color} copying MySql repofile \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log_file
-echo $?
+status_check
+
 
 echo -e "${color} Installing MySql Server \e[0m"
 dnf install mysql-community-server -y &>>log_file
-echo $?
+status_check
 
 echo -e "${color} enabling MySql Server\e[0m"
 systemctl enable mysqld &>>log_file
 systemctl start mysqld &>>log_file
-echo $?
+status_check
 
 echo -e "${color} setting MySql password \e[0m"
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>log_file
-echo $?
+status_check
